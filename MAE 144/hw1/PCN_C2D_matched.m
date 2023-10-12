c33bcd10fad3fa452ef75926b9c02169d6ecb96b
philcnguy <phillip.nguyen031301@gmail.com>
2023-10-12 15:04:47 -0700

    Update PCN_C2D_matched.m

--- MAE 144/hw1/PCN_C2D_matched.m
+++ MAE 144/hw1/PCN_C2D_matched.m
@@ -7,6 +7,11 @@ function [Dz] = PCN_C2D_matched(Ds, h, omega_bar, causality)
 %           causality   Dz(z) either semi-causal or strictly-causal,
 %           default value of strictly-causal
 % OUTPUTS:  Dz controller converted to discrete time
+
+    if nargin < 2
+        h = 0.01;
+    end
+
     if nargin < 3
         omega_bar = 1;
     end
@@ -15,5 +20,34 @@ function [Dz] = PCN_C2D_matched(Ds, h, omega_bar, causality)
         causality = "strictly";
     end
 
-    Dz = 1;
+    % Map zeros & poles
+    s_zeros = Ds.z;
+    s_poles = Ds.p;
+
+    z_zeros = exp(s_zeros * h);
+    z_poles = exp(s_poles * h);
+
+    z_num = RR_poly(z_zeros, 1);
+    z_den = RR_poly(z_poles, 1);
+
+    Dz = RR_tf(z_num, z_den);
+
+    if Dz.num.n < Dz.den.n
+        for i = Dz.num.n:Dz.den.n - 1
+            z_zeros(end + 1) = -1;
+        end
+        z_num = RR_poly(z_zeros, 1);
+    end
+
+    Dz = RR_tf(z_num, z_den);
+
+    if causality == "strictly"
+        if z_zeros(end) == -1
+            z_zeros(end) = [];
+            z_num = RR_poly(z_zeros, 1);
+        end
+    end
+
+    Dz = RR_tf(z_num, z_den);
+
 end
