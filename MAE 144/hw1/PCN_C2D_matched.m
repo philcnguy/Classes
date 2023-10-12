414a293ad6b51d77cc7638f6d695717584514a05
philcnguy <phillip.nguyen031301@gmail.com>
2023-10-12 15:40:04 -0700

    Update PCN_C2D_matched.m

--- MAE 144/hw1/PCN_C2D_matched.m
+++ MAE 144/hw1/PCN_C2D_matched.m
@@ -7,13 +7,13 @@ function [Dz] = PCN_C2D_matched(Ds, h, omega_bar, causality)
 %           causality   Dz(z) either semi-causal or strictly-causal,
 %           default value of strictly-causal
 % OUTPUTS:  Dz controller converted to discrete time
-
+    % Default values
     if nargin < 2
         h = 0.01;
     end
 
     if nargin < 3
-        omega_bar = 1;
+        omega_bar = 0;
     end
 
     if nargin < 4
@@ -32,6 +32,7 @@ function [Dz] = PCN_C2D_matched(Ds, h, omega_bar, causality)
 
     Dz = RR_tf(z_num, z_den);
 
+    % Add zeros at infinity
     if Dz.num.n < Dz.den.n
         for i = Dz.num.n:Dz.den.n - 1
             z_zeros(end + 1) = -1;
@@ -41,13 +42,37 @@ function [Dz] = PCN_C2D_matched(Ds, h, omega_bar, causality)
 
     Dz = RR_tf(z_num, z_den);
 
+    % Remove zeros if strictly-causal
     if causality == "strictly"
         if z_zeros(end) == -1
             z_zeros(end) = [];
             z_num = RR_poly(z_zeros, 1);
+            Dz = RR_tf(z_num, z_den);
         end
     end
 
-    Dz = RR_tf(z_num, z_den);
+    % a/b = k*c/d, where k is DCgain
+    a = 0;
+    b = 0;
+    c = 0;
+    d = 0;
+    for i = Ds.num.n:-1:0
+        a = a + Ds.num.poly(i + 1) * omega_bar ^ (Ds.num.n - i);
+    end
+    
+    for i = Ds.den.n:-1:0
+        b = b + Ds.den.poly(i + 1) * omega_bar ^ (Ds.den.n - i);
+    end
 
+    for i = Dz.num.n:-1:0
+        c = c + Dz.num.poly(i + 1) * exp(omega_bar) ^ (i - Dz.num.n);
+    end
+    
+    for i = Dz.den.n:-1:0
+        d = d + Dz.den.poly(i + 1) * exp(omega_bar) ^ (i - Dz.den.n);
+    end
+
+    DCgain = (a * d) / (b * c);
+
+    Dz = Dz * DCgain;
 end
