b39f074544bd8b5cc945160efbb53b7196d3f7dd
philcnguy <phillip.nguyen031301@gmail.com>
2023-11-16 00:01:51 -0800

    Update hw3_prob1.m

--- MAE_144/hw3/hw3_prob1.m
+++ MAE_144/hw3/hw3_prob1.m
@@ -41,10 +41,51 @@ T_D = gamma * T_u;
 
 D_PID = K_p * T_D * (s^2 + s/T_D + 1/(T_I*T_D)) / s;
 
+omega_I = 1 / T_I;
+omega_D = 1 / T_D;
+
 figure(5)
 rlocus(G_delay * D_PID / (1 + G_delay * D_PID));
 figure(6)
 bode(G_delay * D_PID / (1 + G_delay * D_PID));
+
+%%
+% K_u = 3.2;
+% D_P = K_u;
+% omega_u = 0.3;
+% T_u = 1 / omega_u;
+% K_p = alpha * K_u;
+% T_I = beta * T_u;
+% T_D = gamma * T_u;
+% 
+% D_PID = K_p * T_D * (s^2 + s/T_D + 1/(T_I*T_D)) / s;
+
+G_total = G_delay * D_PID / (1 + G_delay * D_PID);
+
+t = 0:0.01:5;
+
+u = zeros(size(t));
+u(t >= 0 & t < 1) = 35;
+u(t >= 1 & t < 4) = 45;
+u(t >= 4 & t < 5) = 20;
+
+[y, ~, x] = lsim(G_total, u, t);
+
+figure(7)
+plot(t, y + 20, 'LineWidth', 2);
+xlabel('Time (hours)');
+ylabel('Temperature (degrees C)');
+title('Temperature of the Water Bath over Time');
+grid on;
+
+%%
+
+G_delay_discrete = c2d(G_delay,2);
+
+figure(8)
+rlocus(G_delay_discrete);
+figure(9)
+bode(G_delay_discrete);
 
 % 
 % 
