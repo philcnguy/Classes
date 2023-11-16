a34cbab2596e2acf0c6f5e212f8a9055fe3db696
philcnguy <phillip.nguyen031301@gmail.com>
2023-11-15 22:26:02 -0800

    Update hw3

--- MAE_144/hw3/hw3_prob1.m
+++ MAE_144/hw3/hw3_prob1.m
@@ -17,6 +17,50 @@ figure(1)
 bode(G);
 title("Bode Plot with No Delay");
 
-figure(2);
+figure(2)
 bode(G_delay);
 title("Bode Plot with Delay");
+
+alpha = 0.1;
+beta = 5;
+gamma = 1;
+
+K_u = 0.8;
+D_P = K_u;
+
+figure(3)
+rlocus(G_delay * D_P / (1 + G_delay * D_P));
+figure(4)
+bode(G_delay * D_P / (1 + G_delay * D_P))
+
+omega_u = 0.588; % from Bode plot
+T_u = 1 / omega_u;
+K_p = alpha * K_u;
+T_I = beta * T_u;
+T_D = gamma * T_u;
+
+D_PID = K_p * T_D * (s^2 + s/T_D + 1/(T_I*T_D)) / s;
+
+figure(5)
+rlocus(G_delay * D_PID / (1 + G_delay * D_PID));
+figure(6)
+bode(G_delay * D_PID / (1 + G_delay * D_PID));
+
+% 
+% 
+% omega_u = 0.3;
+% 
+% T_u = 1 / omega_u;
+% K_p = alpha * K_u;
+% T_I = beta * T_u;
+% T_D = gamma * T_u;
+% 
+% D_PID = K_p * T_D * (s^2 + s/T_D + 1/(T_I*T_D)) / s;
+% 
+% figure(3)
+% bode(G_delay * D_PID / (1 + G_delay * D_PID));
+% 
+% figure(4)
+% rlocus(G_delay * D_P / (1 + G_delay * D_P));
+% figure(5)
+% bode(G_delay * D_P / (1 + G_delay * D_P))
