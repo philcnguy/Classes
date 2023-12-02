8604eb06e080ea370fbdb8bf6c1931e15ce8d966
philcnguy <phillip.nguyen031301@gmail.com>
2023-12-01 23:15:43 -0800

    Update orbital_simulation.m

--- MAE_207/hw3/orbital_simulation.m
+++ MAE_207/hw3/orbital_simulation.m
@@ -1,26 +1,20 @@
 function orbital_simulation()
 
-    % Parameters
     mu = 1;
     r0 = [1; 0; 0];
     v0 = [0; 1; 0];
     
-    % Values of 'a' to iterate through
     a_values = [0.001, 0.01, 0.1, 1];
 
-    % Time span for integration
-    tspan = [0, 10]; % Adjust as needed
+    tspan = [0, 10];
 
     for a = a_values
         ad = [a; 0; 0];
         
-        % Initial conditions
         y0 = [r0; v0];
 
-        % Numerical integration using ode45
         [t, y] = ode45(@(t, y) odefunc(t, y, mu, ad), tspan, y0);
 
-        % Plot the trajectory
         figure;
         plot3(y(:, 1), y(:, 2), y(:, 3));
         title(['Trajectory for a = ' num2str(a)]);
@@ -29,7 +23,6 @@ function orbital_simulation()
         zlabel('Z');
         grid on;
 
-        % Compute and plot classical orbital elements
         elements = compute_orbital_elements(y, mu);
         plot_orbital_elements(t, elements, a);
     end
@@ -42,7 +35,6 @@ function dydt = odefunc(t, y, mu, ad)
     r_norm = norm(r);
     dydt = zeros(6, 1);
 
-    % Equations of motion
     dydt(1:3) = v;
     dydt(4:6) = -mu / r_norm^3 * r + ad;
 end
@@ -52,14 +44,14 @@ function elements = compute_orbital_elements(y, mu)
     v = y(:, 4:6);
 
     h = cross(r, v);
-    n = cross([zeros(size(h, 1), 2), ones(size(h, 1), 1)], h); % Corrected dimensions
+    n = cross([zeros(size(h, 1), 2), ones(size(h, 1), 1)], h);
 
     r_norm = vecnorm(r, 2, 2);
     v_norm = vecnorm(v, 2, 2);
 
     elements.a = 1 ./ (2 ./ r_norm - v_norm.^2 / mu);
     elements.e = sqrt(1 - (vecnorm(h, 2, 2).^2 / (mu * elements.a)));
-    elements.i = acosd(h(:, 3) ./ vecnorm(h, 2, 2)); % Use vecnorm for correct dimensions
+    elements.i = acosd(h(:, 3) ./ vecnorm(h, 2, 2));
     elements.Omega = atan2d(n(:, 2), n(:, 1));
     elements.omega = atan2d(r(:, 3) ./ sind(elements.i), -r(:, 2) ./ sind(elements.i));
     elements.M = acosd((elements.a .* (1 - elements.e.^2) - r_norm) ./ (elements.e .* r_norm));
