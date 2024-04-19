8f6d47d1f9135202084dc183b24e875737acf05a
philcnguy <phillip.nguyen031301@gmail.com>
2024-04-18 22:49:16 -0700

    Complete problem_2b

--- UCSD/MAE_249/hw1/problem_2.m
+++ UCSD/MAE_249/hw1/problem_2.m
@@ -2,15 +2,28 @@ clear all; clc; clf;
 
 %% Problem 2a
 
-b = 15;
+b1 = 15;
 n = 3;
 l = 10;
 
-theta = linspace(0, pi / 2, 50);
-V = b^3 / (4 * pi * n^2) * sin(theta).^2 .* cos(theta);
+theta_1 = linspace(0, pi / 2, 50);
+V_1 = b1^3 / (4 * pi * n^2) * sin(theta_1).^2 .* cos(theta_1);
 
 figure(1)
-plot(theta,V)
+plot(theta_1,V_1)
+title("Pneumatic Artificial Muscle Volume vs. Angle")
+xlabel("Angle (rad)");
+ylabel("Volume (cm^3)");
+
+%% Problem 2b
+
+b2 = 25;
+
+theta_2 = linspace(0, pi / 2, 50);
+V_2 = b2^3 / (4 * pi * n^2) * sin(theta_2).^2 .* cos(theta_2);
+
+figure(2)
+plot(theta_2,V_2)
 title("Pneumatic Artificial Muscle Volume vs. Angle")
 xlabel("Angle (rad)");
 ylabel("Volume (cm^3)");
